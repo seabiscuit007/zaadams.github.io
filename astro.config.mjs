83210168a066e368215ed3d5e5d8ddba01956627
seabiscuit007 <zadams10@byu.edu>
2026-04-12 23:23:40 -0600

    fix paths for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  site: 'https://seabiscuit007.github.io',
-  base: '/zaadams.github.io',
+  site: 'https://zaadams.com',
+  base: '/',
 });
